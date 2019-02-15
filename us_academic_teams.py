362fecc29913efa20350ab64df5b1c3a1694f1af
Collin Clark <crclark96@gmail.com>
2019-02-15 15:40:42 -0500

    show team scores

--- us_academic_teams.py
+++ us_academic_teams.py
@@ -3,6 +3,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+def get_score(entry):
+  try:
+    return entry.td.next_sibling.next_sibling.next_sibling.string
+  except:
+    return None
+
 headers = {'User-Agent':
            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36\
             (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -13,13 +19,21 @@ soup = BeautifulSoup(us, 'html.parser')
 soup.find_all('tr')
 l = soup.find_all('tr')
 l = l[1:]
-us = [row.a.string for row in l]
+us = [(row.a.string, get_score(row)) for row in l]
 soup = BeautifulSoup(acad, 'html.parser')
 l = soup.find_all('tr')
 l = l[1:]
-acad = [row.a.string for row in l]
+acad = [(row.a.string, get_score(row)) for row in l]
 us_acad = [team for team in acad if team in us]
 
-print(us_acad)
-print()
-print("blue hens is " + str(us_acad.index("Blue Hens") + 1))
+rating = 0
+
+for rank,team in zip(range(len(us_acad)),us_acad):
+  print(team[0] + ": " + team[1])
+  if team[0] == "Blue Hens":
+    rating = rank + 1
+
+print("\nblue hens is: " + str(rating))
+
+
+
